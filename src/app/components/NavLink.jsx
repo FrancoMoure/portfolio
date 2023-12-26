1051f1a04a46e2004757a4c340d6b1e090190a84
Franco Moure <francomoureitschat@gmail.com>
2023-12-26 09:08:15 -0300

    changes again on navbar

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(href);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <Link
+    <a
       href={href}
+      onClick={handleClick}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
       {title}
-    </Link>
+    </a>
   );
 };
 
