9d69433aaa4a8b89b5989b3b8d095f2c7e0d2a74
Franco Moure <francomoureitschat@gmail.com>
2023-12-26 09:03:42 -0300

    changes on navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -26,10 +26,11 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link href="/">
-          <a className="text-2xl md:text-5xl text-white font-semibold">
-            Franco Moure
-          </a>
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          Franco Moure
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
